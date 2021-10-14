7797d8fd6ea54a955508a5fd53925803996ac73b
muhammadabdumalikov <burakali424@gmail.com>
2021-10-15 02:06:18 +0500

    added OrderList and ProductList Screen

--- src/screens/AdminMenuScreen/OrdersScreen/index.jsx
+++ src/screens/AdminMenuScreen/OrdersScreen/index.jsx
@@ -16,8 +16,16 @@ const OrdersScreen = () => {
                     bottom: "10%",
                     width: "45%",
                     left: "2.5%",
-                    borderRadius: 100,
+                    borderRadius: 10,
                     backgroundColor: "white",
+                    elevation: 15, // shadow on Android
+                    shadowRadius: 4, // shadow blur on iOS
+                    shadowOpacity: 0.1, // shadow on iOS,
+                },
+                tabBarLabelStyle: {
+                    textTransform: "capitalize",
+                    fontWeight: "bold",
+                    fontSize: 16
                 },
                 tabBarPressColor: "white",
                 tabBarStyle: {
@@ -26,17 +34,14 @@ const OrdersScreen = () => {
                     alignSelf: "center",
                     justifyContent: "center",
                     width: "90%",
-                    borderRadius: 50,
+                    borderRadius: 10,
                     borderColor: "blue",
                     backgroundColor: "#E5E5E7",
-                    elevation: 5, // shadow on Android
-                    shadowOpacity: 0.1, // shadow on iOS,
-                    shadowRadius: 4, // shadow blur on iOS
                 },
             }}
         >
-            <Tab.Screen name="Moderator" component={ModeratorsScreen} />
-            <Tab.Screen name="Moderator2" component={ModeratorsScreen} />
+            <Tab.Screen name="Order List" component={ModeratorsScreen} />
+            <Tab.Screen name="Product List" component={ModeratorsScreen} />
         </Tab.Navigator>
     );
 };
