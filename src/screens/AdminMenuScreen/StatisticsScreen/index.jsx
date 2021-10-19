26cf568eab3d09e203d9a243c8c3d0dff5dc2d87
muhammadabdumalikov <burakali424@gmail.com>
2021-10-19 17:29:36 +0500

    added Statistics Screen

---
+++ src/screens/AdminMenuScreen/StatisticsScreen/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import styles from "./styles"
+
+const StatisticsScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Cooming soon</Text>
+        </View>
+    );
+};
+
+export default StatisticsScreen;
