1d15e879466cb4d3a5f7f84aced56f959ddd4f62
muhammadabdumalikov <burakali424@gmail.com>
2021-10-20 19:51:33 +0500

    added Notification Screen

--- src/screens/SearchScreen/index.jsx
+++ src/screens/SearchScreen/index.jsx
@@ -1,14 +1,30 @@
-import React from "react"
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 import styles from "./styles";
 
 const SearchScreen = () => {
-    return(
+    const [searchBtnVisible, setSearchBtnVisible] = useState(false);
+    return (
         <View style={styles.container}>
+            <View style={styles.searchBoxWrapper}>
+                <View style={styles.searchBox}>
+                    <Feather name="search" size={18} color="black" />
+                    <TextInput
+                        placeholder="Search"
+                        onFocus={() => setSearchBtnVisible(true)}
+                    />
+                </View>
+                {searchBtnVisible ? (
+                    <TouchableOpacity style={styles.searchBtn} onPress={()=>setSearchBtnVisible(false)}>
+                        <Feather name="x-circle" size={18} color="black" />
+                    </TouchableOpacity>
+                ) : null}
+            </View>
             <Text>This screen unsupported now</Text>
         </View>
-    )
-}
+    );
+};
 
 export default SearchScreen;
--- src/screens/SearchScreen/styles.js
+++ src/screens/SearchScreen/styles.js
@@ -1,10 +1,31 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
+
+const height = Dimensions.get("window").height
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        padding: 16
+    },
+    searchBoxWrapper: {
+        height: height / 22.55,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: "#E5E5E7",
+        borderRadius: 10,
+        padding: 15
+    },
+    searchBox: {
+        height: "100%",
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    searchBtn: {
+        paddingRight: 5,
+        height: "80%",
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
     }
 })
 
